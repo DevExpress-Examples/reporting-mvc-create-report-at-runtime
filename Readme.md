8fac381ba759f82f77dedef0301d8866028912e0
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# How to dynamically generate a report in an ASP.NET MVC application
+
+
+<p>This example illustrates how to create a report in code in an ASP.NET MVC application.<br><br></p>
+<p>The following steps are essential to create a report layout:</p>
+<p>1. Create a report instance and <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument15034">bind it to data</a>.</p>
+<p>2. Add required <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument2590">bands</a> to the report.</p>
+<p>3. Add required <a href="https://documentation.devexpress.com/#XtraReports/CustomDocument2605">controls</a> to the created bands and provide data to them.</p>
+<p>After the report layout is complete, you can generate the report document and display it in a <a href="https://documentation.devexpress.com/#AspNet/CustomDocument10007">Print Preview</a>.</p>
+<p><br><strong>See also</strong>:<br><a href="https://www.devexpress.com/Support/Center/p/E3248">How to use XtraReports in an ASP.NET MVC application</a><br><a href="https://www.devexpress.com/Support/Center/p/T144065">How to link the Document Viewer with an external Ribbon control</a></p>
+
+<br/>
+
+
---
+++ VB/E4714/Models/Category.vb
@@ -0,0 +1,81 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Data
+
+Public Class Category
+	Private privateCategoryID As Integer
+	Public Property CategoryID() As Integer
+		Get
+			Return privateCategoryID
+		End Get
+		Set(ByVal value As Integer)
+			privateCategoryID = value
+		End Set
+	End Property
+
+	Private privateCategoryName As String
+	Public Property CategoryName() As String
+		Get
+			Return privateCategoryName
+		End Get
+		Set(ByVal value As String)
+			privateCategoryName = value
+		End Set
+	End Property
+
+	Private privateDescription As String
+	Public Property Description() As String
+		Get
+			Return privateDescription
+		End Get
+		Set(ByVal value As String)
+			privateDescription = value
+		End Set
+	End Property
+
+	Private privatePicture As Byte()
+	Public Property Picture() As Byte()
+		Get
+			Return privatePicture
+		End Get
+		Set(ByVal value As Byte())
+			privatePicture = value
+		End Set
+	End Property
+
+	Private privateIcon_17 As Byte()
+	Public Property Icon_17() As Byte()
+		Get
+			Return privateIcon_17
+		End Get
+		Set(ByVal value As Byte())
+			privateIcon_17 = value
+		End Set
+	End Property
+
+	Private privateIcon_25 As Byte()
+	Public Property Icon_25() As Byte()
+		Get
+			Return privateIcon_25
+		End Get
+		Set(ByVal value As Byte())
+			privateIcon_25 = value
+		End Set
+	End Property
+
+	Public Shared Function GetCategories() As List(Of Category)
+		Dim catData As DataTable = DataHelper.ProcessSelectCommand("SELECT * FROM [Categories]")
+		If catData IsNot Nothing Then
+			Dim categories As New List(Of Category)()
+			For Each row As DataRow In catData.Rows
+				Dim category As New Category() With {.CategoryID = CInt(Fix(row("CategoryID"))), .CategoryName = CStr(row("CategoryName")), .Description = CStr(row("Description")), .Picture = CType(row("Picture"), Byte()), .Icon_17 = CType(row("Icon_17"), Byte()), .Icon_25 = CType(row("Icon_25"), Byte())}
+				categories.Add(category)
+			Next row
+			Return categories
+		End If
+		Return Nothing
+	End Function
+End Class
